aa0a0681b54ab32e62f77ba71d5dd2a503783b2b
presidentofkazakhstan <alibekbekhan@gmail.com>
2019-03-29 14:36:22 +0600

    commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module16_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> strings = new List<int>();
+            var loadFile = new StreamReader("sumNumbers.txt");
+            string _string = "";
+            while (!loadFile.EndOfStream)
+            {
+                _string = loadFile.ReadToEnd();
+                string[] splitString = _string.Split(' ');
+                for (int i = 0; i < splitString.Count(); i++)
+                {
+                    strings.Add(int.Parse(splitString[i]));
+                }
+            }
+            loadFile.Close();
+            var saveFile = new StreamWriter("sumResults.txt");
+            saveFile.Write(strings.Sum());
+            saveFile.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("module16_homework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("module16_homework")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("10e8fdeb-d662-45e9-90d9-5298346039e2")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
